180c45ca0ee7af08a8df4df64736b68038bac7ad
burntobsidian <me@burntobsidian.com>
2023-11-16 21:22:48 +0530

    add prettier tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Text, View } from "../components/Themed";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function Home() {
   const insets = useSafeAreaInsets();
 
   return (
-    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom, paddingHorizontal: 16 }}>
-      <Text>hello</Text>
-    </View>
+    <ScrollView
+      className="flex-1 flex-row px-4"
+      style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
+    >
+      <View className="flex-1 border-l-2 border-red-500"></View>
+    </ScrollView>
   );
 }
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+
+/** @type { PrettierConfig | TailwindConfig } */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
